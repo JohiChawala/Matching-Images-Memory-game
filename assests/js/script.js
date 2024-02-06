051adeee3610c4a26573439de16eaca5b2529e93
JohiChawala <136928760+JohiChawala@users.noreply.github.com>
2024-02-06 12:08:27 +0500

    adding javascript

--- assests/js/script.js
+++ assests/js/script.js
@@ -128,9 +128,9 @@ const matrixGenerator = (cardValues, size = 4) => {
             <h4>Moves: ${movesCount}</h4>`;
               stopGame();
             }
-          } else {
-            //if the cards dont match
-            //flip the cards back to normal
+          } else 
+          {
+            //if the cards dont match flip the cards back to normal
             let [tempFirst, tempSecond] = [firstCard, secondCard];
             firstCard = false;
             secondCard = false;
